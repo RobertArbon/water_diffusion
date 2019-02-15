049ffa8d69788ff1f697aeba3a5725dc0a3926c9
Robert Arbon <robert.arbon@bristol.ac.uk>
2019-02-15 17:23:57 +0000

    Redone traj 4 analysis

---
+++ data/get_nojumps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#for j in {3..10}
+#do
+j=3
+scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/traj_"$j".xtc .
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/traj_"$j".pdb .
+#done
