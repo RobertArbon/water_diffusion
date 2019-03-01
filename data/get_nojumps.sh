9eef44c2d309c6f667e66fcf259066565ebe3ce0
Robert Arbon <robert.arbon@bristol.ac.uk>
2019-03-01 14:25:55 +0000

    Clean up of file names.  Added some new traj 3 analysis

--- data/get_nojumps.sh
+++ data/get_nojumps.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#for j in {3..10}
-#do
+## No jumps trajectories
 j=3
-scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/traj_"$j".xtc .
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/traj_"$j".xtc .
 #scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/traj_"$j".pdb .
-#done
+
+## Frozen water trajectories
+
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/water_only/4_300ns_300K_just-water.xtc .
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/water_only/4_400ns_300K_just-water.xtc .
+scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_152ns_com_positions.csv .
+scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_151ns_com_positions.csv .
+
+
