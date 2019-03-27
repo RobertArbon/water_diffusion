ebcbea18b519a4612fc21754333534b242636289
Robert Arbon <robert.arbon@bristol.ac.uk>
2019-03-27 20:32:44 +0000

    Updating tica analysis for traj 3

--- data/get_nojumps.sh
+++ data/get_nojumps.sh
@@ -9,7 +9,8 @@ j=3
 
 #scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/water_only/4_300ns_300K_just-water.xtc .
 #scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/water_only/4_400ns_300K_just-water.xtc .
-scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_152ns_com_positions.csv .
-scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_151ns_com_positions.csv .
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_152ns_com_positions.csv .
+#scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/4/4_151ns_com_positions.csv .
+scp ra15808@bluecrystalp3.bris.ac.uk:/projects/dynamics_of_glassy_aerosols/water_only/4_154ns_400K_just-water.xtc .
 
 
